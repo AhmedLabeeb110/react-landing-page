f70253f98034c87f01c97e38f9d8497bdd469613
Ahmed Labeeb <68148397+AhmedLabeeb110@users.noreply.github.com>
2021-11-06 04:30:52 +0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./Components/NavBar";
+import BannerItems from "./Components/BannerItems";
+import About from "./Components/About.js";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <BannerItems />
+      <About />
+    </div>
+  );
+}
+
+export default App;
